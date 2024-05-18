b326a79c48757385be76b0c9004ca81f264d8046
devlucaslima <developerlucaslima@gmail.com>
2024-05-18 12:45:57 -0300

    ✨feat: sign up route fixed up

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -27,7 +28,7 @@ export const router = createBrowserRouter([
       },
       {
         path: '/sign-up',
-        element: <SignIn />,
+        element: <SignUp />,
       },
     ],
   },
