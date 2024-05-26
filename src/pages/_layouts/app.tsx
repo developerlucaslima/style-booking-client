9a7be2d371c12f35366bfaf37b2e12f21886bbc4
devlucaslima <developerlucaslima@gmail.com>
2024-05-26 17:23:58 -0300

    ✨ feat: app layout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -3,11 +3,7 @@ import { Outlet } from 'react-router-dom'
 export function AppLayout() {
   return (
     <div>
-      <h1>Header</h1>
-
-      <div>
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   )
 }
