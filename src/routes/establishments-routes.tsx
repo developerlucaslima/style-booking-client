9dabbcb10285b76fc28155bd2c0756c1451f0037
devlucaslima <developerlucaslima@gmail.com>
2024-09-02 14:41:56 -0300

    :wastebasket: remove files

--- src/routes/establishments-routes.tsx
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from 'react-router-dom'
-
-import { Dashboard } from '@/pages/establishments/app/dashboard'
-import { SignIn } from '@/pages/establishments/auth/sign-in'
-
-export const establishmentRoutes: RouteObject[] = [
-  { path: '/', element: <SignIn /> },
-  { path: '/dashboard', element: <Dashboard /> },
-]
