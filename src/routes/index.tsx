6c2d02081fb3e1702dbf3b98471c0bf5d41edc98
devlucaslima <developerlucaslima@gmail.com>
2024-09-02 12:01:06 -0300

    :wastebasket: remove files

--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-// src/router.tsx
-import { createBrowserRouter } from 'react-router-dom'
-
-import { establishmentRoutes } from './establishments-routes'
-import { usersRoutes } from './users-routes'
-
-const routes = [...establishmentRoutes, ...usersRoutes]
-
-export const router = createBrowserRouter(routes)
