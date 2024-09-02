d16c86dfdbc683884c2d1f07972a4ccdfbe979bb
devlucaslima <developerlucaslima@gmail.com>
2024-09-02 11:51:42 -0300

    :sparkles: feat: initial routes

---
+++ src/routes/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+// import { establishmentRoutes } from './establishments-routes'
+import { usersRoutes } from './users-routes'
+
+const routes = [
+  // ...establishmentRoutes,
+  ...usersRoutes,
+]
+
+export const router = createBrowserRouter(routes)
--- src/routes/users-routes.tsx
+++ src/routes/users-routes.tsx
@@ -1,16 +1,27 @@
 import { RouteObject } from 'react-router-dom'
 
 import { Dashboard } from '@/pages/users/app/dashboard'
-import { SignIn } from '@/pages/users/auth/sign-in'
+import { UserSignIn } from '@/pages/users/auth/user-sign-in'
+import { UserSignUp } from '@/pages/users/auth/user-sign-up'
 import { AppLayout } from '@/pages/users/layouts/app'
+import { AuthLayout } from '@/pages/users/layouts/auth'
 
 export const usersRoutes: RouteObject[] = [
   {
     path: '/',
+    element: <AuthLayout />,
+    children: [
+      { path: '/', element: <UserSignIn /> },
+      { path: '/sign-in', element: <UserSignIn /> },
+      { path: '/sign-up', element: <UserSignUp /> },
+    ],
+  },
+  {
+    path: '/',
     element: <AppLayout />,
     children: [
-      { path: '/', element: <SignIn /> },
       { path: '/dashboard', element: <Dashboard /> },
+      // { path: '/', element: <SignIn /> },
     ],
   },
 ]
