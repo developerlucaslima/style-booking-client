42cfd1e8ea3d992a6f266be2b0c568f3200e3311
developerlucaslima <developerlucaslima@gmail.com>
2025-03-21 11:39:37 -0300

    :sparkles: feat: update UserAppLayout to include Header component and improve layout structure

--- src/pages/user/user-layouts/user-app-layout.tsx
+++ src/pages/user/user-layouts/user-app-layout.tsx
@@ -1,16 +1,17 @@
+import { Header } from '@/components/header'
+import { useAppRoutes } from '@/hooks/use-app-routes'
 import { Outlet } from 'react-router-dom'
 
-import { useAppRoutes } from '@/hooks/use-app-routes'
-
 export function UserAppLayout() {
-  useAppRoutes({ allowedRoles: ['USER'] })
+	useAppRoutes({ allowedRoles: ['USER'] })
 
-  return (
-    <>
-      <h1>AppLayout</h1>
-      <>
-        <Outlet />
-      </>
-    </>
-  )
+	return (
+		<div className='flex min-h-screen flex-col antialiased'>
+			<Header />
+
+			<div className='flex flex-1 flex-col gap-4 p-8 pt-6'>
+				<Outlet />
+			</div>
+		</div>
+	)
 }
