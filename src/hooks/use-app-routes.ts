dd880de0d45866780e57a4e95ed7e6dadbaaa320
developerlucaslima <developerlucaslima@gmail.com>
2025-03-21 11:41:24 -0300

    style: format code for consistency in use-app-routes and use-auth-routes hooks

--- src/hooks/use-app-routes.ts
+++ src/hooks/use-app-routes.ts
@@ -4,18 +4,19 @@ import { useNavigate } from 'react-router-dom'
 import { useAuthStore } from '@/stores/use-auth-store'
 
 interface AllowedRoles {
-  allowedRoles: string[]
+	allowedRoles: string[]
 }
 
 export function useAppRoutes({ allowedRoles }: AllowedRoles) {
-  const navigate = useNavigate()
-  const { token, role } = useAuthStore()
+	const navigate = useNavigate()
+	const { token, role } = useAuthStore()
 
-  useEffect(() => {
-    if (!token) {
-      navigate('/sign-in', { replace: true })
-    } else if (!allowedRoles.includes(role!)) {
-      navigate('/access-denied', { replace: true })
-    }
-  }, [token, role, allowedRoles, navigate])
+	useEffect(() => {
+		if (!token) {
+			navigate('/sign-in', { replace: true })
+		}
+		if (role && !allowedRoles.includes(role)) {
+			navigate('/access-denied', { replace: true })
+		}
+	}, [token, role, allowedRoles, navigate])
 }
--- src/hooks/use-auth-routes.ts
+++ src/hooks/use-auth-routes.ts
@@ -4,14 +4,14 @@ import { useNavigate } from 'react-router-dom'
 import { useAuthStore } from '@/stores/use-auth-store'
 
 export function useAuthRoutes() {
-  const navigate = useNavigate()
-  const { token } = useAuthStore()
+	const navigate = useNavigate()
+	const { token } = useAuthStore()
 
-  useEffect(() => {
-    if (token) {
-      navigate('/', { replace: true })
-    } else {
-      navigate('/sign-in', { replace: true })
-    }
-  }, [token, navigate])
+	useEffect(() => {
+		if (token) {
+			navigate('/', { replace: true })
+		} else {
+			navigate('/sign-in', { replace: true })
+		}
+	}, [token, navigate])
 }
