b7295cc0b8d927173a1d2ccdae9e18a48585864a
developerlucaslima <developerlucaslima@gmail.com>
2025-03-21 11:43:38 -0300

    :art: refactor: clean up user registration API code for improved readability

--- src/api/user-register.ts
+++ src/api/user-register.ts
@@ -1,20 +1,15 @@
+import type { ServiceGender } from '@/config/enum-types'
 import { api } from '@/lib/axios'
 
-export type ServiceGender = 'MALE' | 'FEMALE' | 'BOTH'
 export interface UserRegisterBody {
-  name: string
-  email: string
-  password: string
-  serviceGender: ServiceGender
+	name: string
+	email: string
+	password: string
+	serviceGender: ServiceGender
 }
 
-export async function userRegister({
-  name,
-  email,
-  password,
-  serviceGender,
-}: UserRegisterBody) {
-  console.log({ name, email, password, serviceGender })
+export async function userRegister({ name, email, password, serviceGender }: UserRegisterBody) {
+	console.log({ name, email, password, serviceGender })
 
-  await api.post('/user-register', { name, email, password, serviceGender })
+	await api.post('/user-register', { name, email, password, serviceGender })
 }
