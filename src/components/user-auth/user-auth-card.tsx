c3c651163f4ae95965a2fd481cb6f1122576b95c
developerlucaslima <developerlucaslima@gmail.com>
2025-03-21 11:42:42 -0300

    style: refactor user-auth components for improved readability and consistency

--- src/components/user-auth/user-auth-card.tsx
+++ src/components/user-auth/user-auth-card.tsx
@@ -1,28 +1,24 @@
-import { ReactNode } from 'react'
+import type { ReactNode } from 'react'
 
 import logo from '@/assets/logo.svg'
 
 import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card'
 
 interface AuthCardProps {
-  title: string
-  description?: string
-  children: ReactNode
+	title: string
+	description?: string
+	children: ReactNode
 }
 
 export function AuthCard({ title, description, children }: AuthCardProps) {
-  return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <img src={logo} alt="Logo" className="h-32 pb-4" />
-        <CardTitle className="text-center">{title}</CardTitle>
-        {description && (
-          <CardDescription className="text-center">
-            {description}
-          </CardDescription>
-        )}
-      </CardHeader>
-      {children}
-    </Card>
-  )
+	return (
+		<Card className='w-[350px]'>
+			<CardHeader>
+				<img src={logo} alt='Logo' className='h-32 pb-4' />
+				<CardTitle className='text-center'>{title}</CardTitle>
+				{description && <CardDescription className='text-center'>{description}</CardDescription>}
+			</CardHeader>
+			{children}
+		</Card>
+	)
 }
--- src/components/user-auth/user-sign-up-form.tsx
+++ src/components/user-auth/user-sign-up-form.tsx
@@ -13,133 +13,129 @@ import { CardContent, CardFooter } from '../ui/card'
 import { Input } from '../ui/input'
 import { Label } from '../ui/label'
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
 } from '../ui/select'
 import { AuthCard } from './user-auth-card'
 
 const signInSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string().min(6),
-  serviceGender: z.enum(['MALE', 'FEMALE', 'BOTH']),
+	name: z.string(),
+	email: z.string().email(),
+	password: z.string().min(6),
+	serviceGender: z.enum(['MALE', 'FEMALE', 'BOTH']),
 })
 type UserSignUpForm = z.infer<typeof signInSchema>
 
 export function UserSignUpForm() {
-  const navigate = useNavigate()
-  const [showPassword, setShowPassword] = useState(false)
-  function togglePasswordVisibility() {
-    setShowPassword(!showPassword)
-  }
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { isSubmitting },
-  } = useForm<UserSignUpForm>()
+	const navigate = useNavigate()
+	const [showPassword, setShowPassword] = useState(false)
+	function togglePasswordVisibility() {
+		setShowPassword(!showPassword)
+	}
+	const {
+		register,
+		handleSubmit,
+		control,
+		formState: { isSubmitting },
+	} = useForm<UserSignUpForm>()
 
-  const { mutateAsync: userRegisterFn } = useMutation({
-    mutationFn: userRegister,
-  })
+	const { mutateAsync: userRegisterFn } = useMutation({
+		mutationFn: userRegister,
+	})
 
-  async function handleSignUp(data: UserSignUpForm) {
-    try {
-      await userRegisterFn({
-        name: data.name,
-        email: data.email,
-        password: data.password,
-        serviceGender: data.serviceGender,
-      })
-      toast.success('Seu cadastro foi feito com sucesso!')
-      navigate(`/sign-in?email=${data.email}`)
-    } catch (error) {
-      toast.error('Erro ao cadastrar restaurante.')
-    }
-  }
+	async function handleSignUp(data: UserSignUpForm) {
+		try {
+			await userRegisterFn({
+				name: data.name,
+				email: data.email,
+				password: data.password,
+				serviceGender: data.serviceGender,
+			})
+			toast.success('Seu cadastro foi feito com sucesso!')
+			navigate(`/sign-in?email=${data.email}`)
+		} catch (_error) {
+			toast.error('Erro ao cadastrar restaurante.')
+		}
+	}
 
-  return (
-    <AuthCard
-      title="Crie grátis sua conta no Estilo Na Web"
-      description="Falta pouca para se sentir ainda mais incrível"
-    >
-      <form
-        onSubmit={handleSubmit(handleSignUp)}
-        className="flex flex-col gap-4"
-      >
-        <CardContent>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Nome</Label>
-              <Input id="name" type="name" {...register('name')} />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">E-mail</Label>
-              <Input id="email" type="email" {...register('email')} />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Senha</Label>
-              <div className="flex gap-1">
-                <Input
-                  id="password"
-                  type={showPassword ? 'text' : 'password'}
-                  {...register('password')}
-                />
-                <Button
-                  onClick={togglePasswordVisibility}
-                  type="button"
-                  size="icon"
-                  variant="ghost"
-                >
-                  {showPassword ? (
-                    <EyeOpenIcon className="h-4 w-4" />
-                  ) : (
-                    <EyeNoneIcon className="h-4 w-4" />
-                  )}
-                </Button>
-              </div>
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Gênero</Label>
-              <div className="flex gap-1">
-                <Controller
-                  control={control}
-                  name="serviceGender"
-                  render={({ field }) => (
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <SelectTrigger>
-                        <SelectValue placeholder="Procura serviços para qual gênero?" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Gênero</SelectLabel>
-                          <SelectItem value="MALE">Masculino</SelectItem>
-                          <SelectItem value="FEMALE">Feminino</SelectItem>
-                          <SelectItem value="BOTH">Unisex</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Já possuo conta</Button>
-          <Button disabled={isSubmitting} type="submit">
-            Finalizar cadastro
-          </Button>
-        </CardFooter>
-      </form>
-    </AuthCard>
-  )
+	return (
+		<AuthCard
+			title='Crie grátis sua conta no Estilo Na Web'
+			description='Falta pouca para se sentir ainda mais incrível'
+		>
+			<form onSubmit={handleSubmit(handleSignUp)} className='flex flex-col gap-4'>
+				<CardContent>
+					<div className='grid w-full items-center gap-4'>
+						<div className='flex flex-col space-y-1.5'>
+							<Label htmlFor='name'>Nome</Label>
+							<Input id='name' type='name' {...register('name')} />
+						</div>
+						<div className='flex flex-col space-y-1.5'>
+							<Label htmlFor='email'>E-mail</Label>
+							<Input id='email' type='email' {...register('email')} />
+						</div>
+						<div className='flex flex-col space-y-1.5'>
+							<Label htmlFor='password'>Senha</Label>
+							<div className='flex gap-1'>
+								<Input
+									id='password'
+									type={showPassword ? 'text' : 'password'}
+									{...register('password')}
+								/>
+								<Button
+									onClick={togglePasswordVisibility}
+									type='button'
+									size='icon'
+									variant='ghost'
+								>
+									{showPassword ? (
+										<EyeOpenIcon className='h-4 w-4' />
+									) : (
+										<EyeNoneIcon className='h-4 w-4' />
+									)}
+								</Button>
+							</div>
+						</div>
+						<div className='flex flex-col space-y-1.5'>
+							<Label htmlFor='password'>Gênero</Label>
+							<div className='flex gap-1'>
+								<Controller
+									control={control}
+									name='serviceGender'
+									render={({ field }) => (
+										<Select onValueChange={field.onChange} defaultValue={field.value}>
+											<SelectTrigger>
+												<SelectValue placeholder='Procura serviços para qual gênero?' />
+											</SelectTrigger>
+											<SelectContent>
+												<SelectGroup>
+													<SelectLabel>Gênero</SelectLabel>
+													<SelectItem value='MALE'>Masculino</SelectItem>
+													<SelectItem value='FEMALE'>Feminino</SelectItem>
+													<SelectItem value='BOTH'>Unisex</SelectItem>
+												</SelectGroup>
+											</SelectContent>
+										</Select>
+									)}
+								/>
+							</div>
+						</div>
+					</div>
+				</CardContent>
+				<CardFooter className='flex justify-between'>
+					<Button variant='outline' onClick={() => navigate('/sign-in')}>
+						Já possuo conta
+					</Button>
+					<Button disabled={isSubmitting} type='submit'>
+						Finalizar cadastro
+					</Button>
+				</CardFooter>
+			</form>
+		</AuthCard>
+	)
 }
