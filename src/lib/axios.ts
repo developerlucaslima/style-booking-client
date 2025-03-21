6e809e4a10fb043f3029b615ff96b585da0c1c3a
developerlucaslima <developerlucaslima@gmail.com>
2025-03-21 11:41:14 -0300

    :sparkles: feat: add request interceptor to include authorization token and enhance error handling for 401 responses

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,37 +1,46 @@
 import axios from 'axios'
-
 import { env } from '@/env'
 import { useAuthStore } from '@/stores/use-auth-store'
 
 export const api = axios.create({
-  baseURL: env.VITE_API_URL,
-  withCredentials: true,
+	baseURL: env.VITE_API_URL,
+	withCredentials: true,
 })
 
+api.interceptors.request.use(
+	(config) => {
+		const token = useAuthStore.getState().token
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`
+			console.log({ token })
+		}
+		return config
+	},
+	(error) => {
+		return Promise.reject(error)
+	},
+)
+
 api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const authStore = useAuthStore()
+	(response) => response,
+	async (error) => {
+		const status = error.response ? error.response.status : null
+		if (status === 401 && !error.config.__isRetryRequest) {
+			try {
+				error.config.__isRetryRequest = true
 
-    // Verifica se o erro é 401 e se não está tentando renovar o token
-    if (error.response.status === 401 && !error.config.__isRetryRequest) {
-      try {
-        // Define que a requisição de refresh token não deve causar um loop de requisições
-        error.config.__isRetryRequest = true
+				const refreshResponse = await api.patch('/token/user-refresh')
+				const { token } = refreshResponse.data
 
-        const refreshResponse = await axios.patch('/token/user-refresh')
-        const { token } = refreshResponse.data
-        authStore.setToken(token)
+				useAuthStore.setState({ token })
 
-        // Atualiza o header Authorization e refaz a requisição original
-        error.config.headers.Authorization = `Bearer ${token}`
-        return api.request(error.config)
-      } catch (refreshError) {
-        authStore.clearAuth()
-        return Promise.reject(refreshError)
-      }
-    }
-
-    return Promise.reject(error)
-  },
+				error.config.headers.Authorization = `Bearer ${token}`
+				return api.request(error.config)
+			} catch (refreshError) {
+				useAuthStore.setState({ token: null, role: null })
+				return Promise.reject(refreshError)
+			}
+		}
+		return Promise.reject(error)
+	},
 )
